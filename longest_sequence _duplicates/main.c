1af132a7ced0bf694ae761f77b8d788b3d1b0232
Armandas <armandas.rokas@gmail.com>
2019-04-03 11:31:09 +0200

    added longest sequence duplicates algorithm

---
+++ longest_sequence _duplicates/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int longest_sequence(int *array, int count)
+{
+    int length;
+    int longest;
+    longest = length = 0;
+    for (int i = 0 ; i < count - 1 ; i++)
+    {
+        if (array[i + 1] == array[i])
+            length++;
+        else
+        {
+             // length + 1 to count the last element too
+            if (longest < length + 1)
+                longest = length + 1;
+            length = 0;
+        }
+    }
+    return longest;
+}
+
+int
+main()
+{
+    int array[] = {2, 2, 4, 4, 4, 4, 4, 5, 2, 2, 1, 3};
+    int count = sizeof(array) / sizeof(array[0]);
+
+    printf("The longest sequence has %d elements\n",
+        longest_sequence(array, count));
+    return 0;
+}
